3ff6a8e524d30acb013aa1e5a175307e12a544be
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-17 15:30:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def run_guessing_game
   computer_random_number = rand(6) + 1
-  puts "Guess number between 1 and 6"
+  #puts "Guess number between 1 and 6"
   input = gets.chomp
     if input == computer_random_number
       puts "You guessed the correct number!"
