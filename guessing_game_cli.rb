b7f6881edb1965b6a7185b15695d177f5d51a16d
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-17 15:21:51 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,6 @@ require 'pry'
 def run_guessing_game
   computer_random_number = rand(6) + 1
   puts "Guess number between 1 and 6"
-  input = gets
-  user_guess = input.chomp
+  input = gets.chomp
 end
 
